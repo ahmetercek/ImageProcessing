339b19efc9acd93a338acbd1d0f622e570fe9e11
aercek <ahmet.ercek@nucleo.com.tr>
2019-11-04 21:13:48 +0300

    Template Designed

---
+++ Histogram_Operations.py
@@ -0,0 +1,23 @@
+from tkinter import *  # Comes Defalut With Python3
+from tkinter import filedialog as fd
+from tkinter import messagebox as ms
+import PIL  # Install Using PIP
+from PIL import ImageTk, Image
+
+
+class Histogram_Operations:
+    'Common base class for all employees'
+
+    def __init__(self):
+        root = Tk()
+        root.configure(bg='white')
+        root.title('Histogram_Operations')
+        root.geometry("1000x500")
+        root.mainloop()
+
+    def displayCount(self):
+        print
+
+
+    def displayEmployee(self):
+        print
